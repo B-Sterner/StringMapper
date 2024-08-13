66b8835ad2e27daa2dbb51a43e294d67f13a4558
B-Sterner <My90Gi-=Hu!b>
2024-08-12 21:24:31 -0600

    Add project files.

---
+++ String Parser/MessageParser.cs
@@ -0,0 +1,103 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using System.Reflection;
+
+namespace String_Parser;
+
+[MemoryDiagnoser]
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+[RankColumn]
+public class MessageParser
+{
+    const string DataString = "Val1=ok1; Val2='ok2'; DecVal=1.21; Enabled; Val4=here;Val3=dog;";
+    private const char breakChar = ';';
+    private const char SplitChar = '=';
+    public DataMessage DataMessage = new();
+
+    public MessageParser()
+    {
+
+    }
+
+    [Benchmark]
+    public DataMessage ParseString()
+    {
+        foreach (var chunk in DataString.Split(breakChar))
+        {
+            var keyValuePair = chunk.Split(SplitChar, StringSplitOptions.TrimEntries);
+
+            if (keyValuePair.Length != 2)
+            {
+                DataMessage.GetType().GetProperty(keyValuePair[0])
+                    ?.SetValue(DataMessage, true);
+            }
+            else if (keyValuePair.Length == 2)
+            {
+                var p = DataMessage.GetType().GetProperty(keyValuePair[0]);
+                if (p != null)
+                {
+                    SetPropertyByType(p, keyValuePair[1]);
+                }
+            }
+
+        }
+        return DataMessage;
+    }
+
+    [Benchmark]
+    public DataMessage ParseStringWithSpan()
+    {
+        ReadOnlySpan<char> spanString = DataString;
+        var start = 0;
+        var end = spanString.IndexOf(breakChar);
+        ReadOnlySpan<char> currentSpan;
+
+        while (start < spanString.Length)
+        {
+            if (end < 0)
+            {
+                currentSpan = spanString[start..];
+                start = spanString.Length;
+            }
+            else
+            {
+                currentSpan = spanString.Slice(start, end);
+                start = start + end + 1;
+            }
+
+            SetProperty(currentSpan);
+            end = spanString[start..].IndexOf(breakChar);
+        }
+        return DataMessage;
+    }
+
+    private void SetProperty(ReadOnlySpan<char> currentSpan)
+    {
+        if (currentSpan.IndexOf(SplitChar) < 0)
+        {
+            DataMessage.GetType().GetProperty(currentSpan.Trim().ToString())
+                ?.SetValue(DataMessage, true);
+        }
+        else
+        {
+            var p = DataMessage.GetType().GetProperty(currentSpan[..currentSpan.IndexOf(SplitChar)].Trim().ToString());
+            if (p != null)
+            {
+                SetPropertyByType(p, currentSpan[(currentSpan.IndexOf(SplitChar) + 1)..].Trim().ToString());
+            }
+        }
+    }
+
+    private void SetPropertyByType(PropertyInfo p, string v)
+    {
+        var t = p.PropertyType.Name;
+        if (t == "Decimal")
+        {
+            p.SetValue(DataMessage, Decimal.Parse(v));
+        }
+        else
+        {
+            p.SetValue(DataMessage, v);
+        }
+    }
+}
---
+++ String Parser/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using BenchmarkDotNet.Running;
+using Microsoft.Diagnostics.Runtime.Utilities;
+using String_Parser;
+using System.Security.Cryptography.X509Certificates;
+
+
+//BenchmarkRunner.Run<MessageParser>();
+
+var stringParser = new MessageParser();
+var result = stringParser.ParseString();
+var result2 = stringParser.ParseStringWithSpan();
+
+
+Console.WriteLine(result.Val1);
+Console.WriteLine(result.Val2);
+Console.WriteLine(result.Val3);
+Console.WriteLine(result.Val4);
+Console.WriteLine(result.DecVal);
+Console.WriteLine(result.IsEnabled);
+
+Console.WriteLine("");
+
+Console.WriteLine(result2.Val1);
+Console.WriteLine(result2.Val2);
+Console.WriteLine(result2.Val3);
+Console.WriteLine(result2.Val4);
+Console.WriteLine(result2.DecVal);
+Console.WriteLine(result2.IsEnabled);
